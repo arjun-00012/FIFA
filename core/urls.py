82fa92602314d41420852f543130553702a4a1e8
arjun-00012 <153722491+arjun-00012@users.noreply.github.com>
2026-06-18 15:28:20 +0530

    Force Django to serve active media images on production containers

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve  
+from django.urls import re_path
 
 urlpatterns = [
     # Django base system management access route portal
@@ -12,5 +14,10 @@ urlpatterns = [
 ]
 
 # CRITICAL FOR FILE PICKERS: Tells Django how to find and render images uploaded from your system
-if settings.DEBUG:
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+else:
+    # Local development server route
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
